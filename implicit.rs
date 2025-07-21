6839f86f087ca46d6e7b0b51697df61d08e721b0
InturirishyendraNag <154305200+InturirishyendraNag@users.noreply.github.com>
2025-07-21 09:57:15 +0530

    Create implicit.rs

---
+++ implicit.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let implicit_var = 42;  
+    let implicit_float = 3.14;  
+    println!("Implicit integer: {}", implicit_var);
+    println!("Implicit float: {}", implicit_float);
+}
