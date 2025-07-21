fa4c2a99637b68c766cc2a0a412f7fb344d1b96f
InturirishyendraNag <154305200+InturirishyendraNag@users.noreply.github.com>
2025-07-21 09:57:50 +0530

    Create explicit.rs

---
+++ explicit.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let explicit_var: i64 = 100;
+    let explicit_float: f32 = 2.718;
+    println!("Explicit integer (i64): {}", explicit_var);
+    println!("Explicit float (f32): {}", explicit_float);
+}
