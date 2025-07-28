21f38cdbb8918340ec24cd74387dccf2371ffe2c
InturirishyendraNag <154305200+InturirishyendraNag@users.noreply.github.com>
2025-07-28 09:35:46 +0530

    Create  Constants in Rust.rs

---
+++ Constants in Rust.rs
@@ -0,0 +1,9 @@
+const PI: f64 = 3.14159;
+const GRAVITY: f64 = 9.8;
+const MAX_SPEED: i32 = 299792458;
+
+fn main() {
+    println!("PI: {}", PI);
+    println!("Gravity: {}", GRAVITY);
+    println!("Max Speed (Speed of Light): {}", MAX_SPEED);
+}
