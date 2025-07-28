706a09944e06356403d9f8d55d71d5236d2ca8ad
InturirishyendraNag <154305200+InturirishyendraNag@users.noreply.github.com>
2025-07-28 09:36:33 +0530

    Create String Literal and String Object.rs

---
+++ String Literal and String Object.rs
@@ -0,0 +1,6 @@
+fn main() {
+    let str_literal = "Hello, World!";
+    let str_object = String::from(str_literal);
+    println!("String Literal: {}", str_literal);
+    println!("String Object: {}", str_object);
+}
